a463765d0448db0bd778d369fcfc8ad4f5ef212d
James <james.welbes@gmail.com>
2026-02-23 14:59:32 -0600

    feat: add Edit Content and Quiz buttons to lessons in Edit Module modal

--- assets/js/learnkit-admin.asset.php
+++ assets/js/learnkit-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '29ffca25a3ffe267b1c8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2e3dc7731b4d5b806a3b');
