2ba8328bd564c73e3304e32840fbe4e19febc970
James <james.welbes@gmail.com>
2026-02-24 11:27:05 -0600

    fix: pass onReorderLessons through CourseDetailModal to CourseStructure

--- assets/js/learnkit-admin.asset.php
+++ assets/js/learnkit-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1ebff8631ca1449e658a');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5b95288126eabe93db88');
